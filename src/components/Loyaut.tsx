24670f95e0e39204ff4318c69dee0e3a5816cde9
Heorhii "Aytivets" Aksiuk <aksiuk.heorhii@gmail.com>
2023-12-09 21:07:11 +0200

    create Loyaut component

---
+++ src/components/Loyaut.tsx
@@ -0,0 +1,9 @@
+import React, {ReactNode} from "react";
+
+interface Props { 
+  children: ReactNode;
+};
+
+export default function Loyaut({children} :Props){
+  return <section>{chicken}</section>;
+};
