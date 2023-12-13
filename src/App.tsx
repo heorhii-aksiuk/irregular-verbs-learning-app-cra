15767c04e89b1fb1976ffa1fac19e4c285d46a0c
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2023-12-13 21:22:49 +0200

    reset all

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { light, dark } from './theme';
-import Layout from './components/Layout';
-import useLocalStorage from './hooks/useLocalStorage';
-import GlobalStyle from './styles/GlobalStyle';
-import { THEME, defaultTheme } from './constants';
-import { ThemeProvider } from 'styled-components';
-import ThemeSwitch from './components/ThemeSwitch';
-
 function App() {
-  const [theme, setTheme] = useLocalStorage(THEME.LS_KEY, THEME.LIGHT);
-
-  const handleSwitchTheme = (isSwitched: boolean) => {
-    const theme = isSwitched ? THEME.LIGHT : THEME.DARK;
-    setTheme(theme);
-  };
-
-  const currentTheme = theme === defaultTheme ? dark : light;
   return (
     <>
-      <ThemeProvider theme={currentTheme}></ThemeProvider>
-      <GlobalStyle />
-      <Layout>
-        <ThemeSwitch
-          onSwitch={handleSwitchTheme}
-          current={theme === defaultTheme}
-        />
-        <div style={{ color: '#fff' }}>App</div>;
-      </Layout>
+      <div>App</div>
     </>
   );
 }
