b476047beca622d2fb1fe20669feac9bd59fcd95
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2024-02-02 23:45:17 +0200

    footer refactoring

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,28 +5,36 @@ import { SiGmail } from 'react-icons/si';
 export default function Footer() {
   /* TODO: footer */
   return (
-    <footer className="h-12 bg-gray-800/90  w-full text-zinc-200 border-b border-gray-900 drop-shadow-md flex justify-center items-center space-x-2">
-      <a
-        href="https://www.linkedin.com/in/heorhii-aksiuk/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaLinkedin size={30} />
-      </a>
-      <a
-        href="https://github.com/heorhii-aksiuk"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaGithub size={30} />
-      </a>
-      <a
-        href="mailto:aksiuk.heorhii@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <SiGmail size={30} />
-      </a>
+    <footer className="h-12 bg-gray-800/90  w-full text-zinc-200 border-b border-gray-900 drop-shadow-md flex justify-center items-center">
+      <ul className="flex space-x-2">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/heorhii-aksiuk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={30} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/heorhii-aksiuk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={30} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:aksiuk.heorhii@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiGmail size={30} />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
